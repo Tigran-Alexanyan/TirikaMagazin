7d62afad7c321813932aad6ba19c650ac76fdbac
Tigran-Alexanyan <alex.yan.19@mail.ru>
2020-02-04 18:27:48 +0400

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent} from './menu/menu.component';
+import { TextComponent} from './text/text.component';
+import { OrderListComponent} from './order-list/order-list.component';
+import { CategoryComponent} from './category/category.component';
+import { ItemsListComponent} from './items-list/items-list.component';
+import { LoginComponent} from './login/login.component';
+import { RegisterComponent} from './register/register.component';
+import {OptionComponent} from './option/option.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'text', component: TextComponent},
+  {path: 'order', component: OrderListComponent},
+  {path: 'category', component: CategoryComponent},
+  {path: 'items', component: ItemsListComponent},
+  {path: 'option', component: OptionComponent},
+  // {path: '**', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { TextComponent } from './text/text.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { CategoryComponent } from './category/category.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { OptionComponent } from './option/option.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TextComponent,
+    OrderListComponent,
+    CategoryComponent,
+    ItemsListComponent,
+    LoginComponent,
+    RegisterComponent,
+    OptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
